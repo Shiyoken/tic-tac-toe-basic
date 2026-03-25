80002ef4166f1912cde0b12fc2f268e967a8aa75
Shiyoken <ngoctracvt1974@gmail.com>
2026-03-25 14:58:33 +0700

    Erase the id attribute, maybe later will modify

--- src/main/java/vgu/trac/ttt/basic/Computer.java
+++ src/main/java/vgu/trac/ttt/basic/Computer.java
@@ -2,16 +2,11 @@ package vgu.trac.ttt.basic;
 
 public class Computer extends Player {
     private int i = 1;
-    
-    public Computer (){
-        id = 2;
-    }
-    
     @Override
     public void makeMove(Board board) {
         System.out.println("Player2 turn");
         for (int index = i ; index <= 9; index++){
-            if (board.placeMove(index, this.id) == 1) {
+            if (board.placeMove(index, 2) == 1) {
                 break;
             }
         }
