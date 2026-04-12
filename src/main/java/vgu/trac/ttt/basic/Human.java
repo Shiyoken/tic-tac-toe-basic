6e48f5b57895f7ee5a44869e23a67bdf397479d7
Shiyoken <ngoctracvt1974@gmail.com>
2026-04-12 21:29:48 +0700

    Adding a Scanner for easy testing (simulate user input), make move now return -1 for invalid/fail makeMove or the position of the cell that users type

--- src/main/java/vgu/trac/ttt/basic/Human.java
+++ src/main/java/vgu/trac/ttt/basic/Human.java
@@ -1,29 +1,49 @@
 package vgu.trac.ttt.basic;
+import java.io.InputStream;
+
 import java.util.*;
 
 public class Human extends Player {
     protected int id;
+    private Scanner keyboard;
 
     public Human() {
         this.id = 1;
+        keyboard = new Scanner(System.in);
     }
-    public void makeMove(Board board){
-        Scanner keyboard = new Scanner(System.in);
-        boolean validMove = false;
-        while (!validMove){
-            System.out.println("Player1 turn");
-            int move = keyboard.nextInt();
-            int result = board.placeMove(move, this.id);
-            if (result == 1) {
-                validMove = true;
+    //For testing
+    public Human(InputStream in) {
+        this.id = 1;
+        keyboard = new Scanner(in);
+    }
+    public int makeMove(Board board){
+        String move = keyboard.nextLine();
+        int moveInt = -1;
+        try {
+            moveInt = Integer.parseInt(move);
+            return moveInt;
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter only numbers");
+            return moveInt;
+        } 
+    }
+
+    //For testing
+    public int makeMove(Board board, InputStream in) {
+        keyboard = new Scanner(in);
+        String move = keyboard.nextLine();
+        int moveInt = -1;
+        try {
+            moveInt = Integer.parseInt(move);
+            if (!board.isValid(moveInt)) {
+                System.out.println("Invalid range, please input numbers from [1-9]");
+                return -1;
             }
-            else if (result == 0) {
-                System.out.println("Occupied. Retry");
-            }
-            else if (result == -1) {
-                System.out.println("Invalid Range");
-            }
+            return moveInt;
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter only numbers");
+            return moveInt;
         }
-        // keyboard.close();   
     }
 }
+
