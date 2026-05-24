edeaef7cef528502c53c5369d149faef847e73ac
Shiyoken <ngoctracvt1974@gmail.com>
2026-05-24 11:45:02 +0700

    Modify the terminal message

--- src/main/java/vgu/trac/ttt/basic/App.java
+++ src/main/java/vgu/trac/ttt/basic/App.java
@@ -7,18 +7,19 @@ public class App {
             return;
         }
         try {
-            int firstPlayer = Integer.parseInt(args[0]);
-
-            if (firstPlayer != 1 && firstPlayer != 2) {
+            // int firstPlayer = Integer.parseInt(args[0]);
+            String firstPlayer = args[0];
+            if (!firstPlayer.equals("1") && !firstPlayer.equals("2")) {
                 System.out.println("Please, input a valid option [1-2]");
                 return;
             }
             else {
-                Game newGame = new Game(firstPlayer);
+                int first = Integer.parseInt(firstPlayer); // Who plays first? 1 or 2
+                Game newGame = new Game(first);
                 newGame.start();
             }
         } catch (NumberFormatException e) {
-            System.out.println("Input a number, not a string");
+            System.out.println("Please, input a valid option [1-2]");
         }
     }
 }
