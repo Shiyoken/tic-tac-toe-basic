12021fe4208adab6eabad6d80ee6309ad3051e42
Shiyoken <ngoctracvt1974@gmail.com>
2026-05-24 11:48:51 +0700

    Add a constructor, so that modified I/O for Human, Board can redirect the ByteStream

--- src/main/java/vgu/trac/ttt/basic/Game.java
+++ src/main/java/vgu/trac/ttt/basic/Game.java
@@ -1,5 +1,7 @@
 package vgu.trac.ttt.basic;
 
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
 
 public class Game {
 
@@ -17,6 +19,14 @@ public class Game {
         this.gameActive = true;
         this.currentPlayerId = firstPlayer;
     }
+    // Constructor for Client-Server Instructor
+    public Game(int firstPlayer, Board board, Human human) {
+        this.board = board;
+        this.human = human;
+        this.computer = new Computer();
+        this.gameActive = true;
+        this.currentPlayerId = firstPlayer;
+    }
 
     public void start() {
         System.out.println("Hello!");
@@ -28,23 +38,28 @@ public class Game {
             currentPlayer = computer;
         }
 
+        // Thread gameThread = new Thread(
+        // () -> {
         while (gameActive) {
-            System.out.println("Player# " + currentPlayerId + "'s turn");
+            System.out.println("Player#" + currentPlayerId + "'s turn");
             int moveCheck = currentPlayer.makeMove(board);
 
-            if (moveCheck == -1) {}
-            else if (!board.isValid(moveCheck)) {
+            if (moveCheck == -2) {
+                System.out.println("End of the game");
+                gameActive = false;
+                return;
+            } else if (moveCheck == 0) {
                 System.out.println("Please, input a valid number [1-9]");
-            }
-            else if (!board.isEmpty(moveCheck)) {
+            } else if (!board.isValid(moveCheck)) {
+                System.out.println("Please, input a valid number [1-9]");
+            } else if (!board.isEmpty(moveCheck)) {
                 System.out.println("The cell is occupied!");
-            }
-            else {
+            } else {
                 board.placeMove(moveCheck, currentPlayerId);
                 int winner = board.isWin();
                 if (winner != 0) {
                     board.printBoard();
-                    System.out.println("Player# " + currentPlayerId + " won!");
+                    System.out.println("Player#" + currentPlayerId + " won!");
                     gameActive = false;
                 } else if (board.isFull()) {
                     board.printBoard();
@@ -62,5 +77,42 @@ public class Game {
                 }
             }
         }
+        // });
+        // gameThread.start();
+
+        // while (gameActive) {
+        // System.out.println("Player# " + currentPlayerId + "'s turn");
+        // int moveCheck = currentPlayer.makeMove(board);
+
+        // if (moveCheck == -1) {}
+        // else if (!board.isValid(moveCheck)) {
+        // System.out.println("Please, input a valid number [1-9]");
+        // }
+        // else if (!board.isEmpty(moveCheck)) {
+        // System.out.println("The cell is occupied!");
+        // }
+        // else {
+        // board.placeMove(moveCheck, currentPlayerId);
+        // int winner = board.isWin();
+        // if (winner != 0) {
+        // board.printBoard();
+        // System.out.println("Player# " + currentPlayerId + " won!");
+        // gameActive = false;
+        // } else if (board.isFull()) {
+        // board.printBoard();
+        // System.out.println("It is a draw!");
+        // gameActive = false;
+        // } else {
+        // board.printBoard();
+        // if (currentPlayerId == 1) {
+        // currentPlayerId = 2;
+        // currentPlayer = computer;
+        // } else {
+        // currentPlayerId = 1;
+        // currentPlayer = human;
+        // }
+        // }
+        // }
+        // }
     }
 }
