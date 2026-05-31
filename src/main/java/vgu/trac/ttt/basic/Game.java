09f47a17a1ec99de76184456c0a9274c6858ba80
Shiyoken <ngoctracvt1974@gmail.com>
2026-05-31 15:41:33 +0700

    Change the System.out -> The OutputStream, so that we can redirect the data byte stream outside eeasily

--- src/main/java/vgu/trac/ttt/basic/Game.java
+++ src/main/java/vgu/trac/ttt/basic/Game.java
@@ -1,5 +1,7 @@
 package vgu.trac.ttt.basic;
 
+import java.io.PrintStream;
+import java.io.OutputStream;
 // import java.util.logging.Level;
 // import java.util.logging.Logger;
 
@@ -12,14 +14,26 @@ public class Game {
     private Player currentPlayer;
     private int currentPlayerId;
 
+    private PrintStream printer;
+
     public Game(int firstPlayer) {
         this.board = new Board_2D();
         this.human = new Human();
         this.computer = new Computer();
         this.gameActive = true;
         this.currentPlayerId = firstPlayer;
+        this.printer = new PrintStream(System.out);
     }
     // Constructor for Client-Server Instructor
+    // public Game(int firstPlayer, Board board, Player firstPlayer, Player secondPlayer) {
+    //     this.board = board;
+    //     this.human = human;
+    //     this.computer = new Computer();
+    //     this.gameActive = true;
+    //     this.currentPlayerId = firstPlayer;
+    //     this.firstPlayer = firstPlayer;
+    // }
+    // Constructor for Client-Server (Single)
     public Game(int firstPlayer, Board board, Human human) {
         this.board = board;
         this.human = human;
@@ -27,9 +41,19 @@ public class Game {
         this.gameActive = true;
         this.currentPlayerId = firstPlayer;
     }
+    // Constructor for Client-Server (Multi)
+    public Game(int firstPlayer, Board board, Human human, OutputStream out) {
+        this.board = board;
+        this.human = human;
+        this.computer = new Computer();
+        this.gameActive = true;
+        this.currentPlayerId = firstPlayer;
+        this.printer = new PrintStream(out);
+    }
+
 
     public void start() {
-        System.out.println("Hello!");
+        printer.println("Hello!");
         board.printBoard();
 
         if (currentPlayerId == 1) {
@@ -41,29 +65,29 @@ public class Game {
         // Thread gameThread = new Thread(
         // () -> {
         while (gameActive) {
-            System.out.println("Player#" + currentPlayerId + "'s turn");
+            printer.println("Player#" + currentPlayerId + "'s turn");
             int moveCheck = currentPlayer.makeMove(board);
 
             if (moveCheck == -2) {
-                System.out.println("End of the game");
+                printer.println("End of the game");
                 gameActive = false;
                 return;
             } else if (moveCheck == 0) {
-                System.out.println("Please, input a valid number [1-9]");
+                printer.println("Please, input a valid number [1-9]");
             } else if (!board.isValid(moveCheck)) {
-                System.out.println("Please, input a valid number [1-9]");
+                printer.println("Please, input a valid number [1-9]");
             } else if (!board.isEmpty(moveCheck)) {
-                System.out.println("The cell is occupied!");
+                printer.println("The cell is occupied!");
             } else {
                 board.placeMove(moveCheck, currentPlayerId);
                 int winner = board.isWin();
                 if (winner != 0) {
                     board.printBoard();
-                    System.out.println("Player#" + currentPlayerId + " won!");
+                    printer.println("Player#" + currentPlayerId + " won!");
                     gameActive = false;
                 } else if (board.isFull()) {
                     board.printBoard();
-                    System.out.println("It is a draw!");
+                    printer.println("It is a draw!");
                     gameActive = false;
                 } else {
                     board.printBoard();
